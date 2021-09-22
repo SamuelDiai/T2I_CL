9d9b963c7963a151d072a8c40e892770d84f22d8
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2021-09-22 14:52:08 +0200

    Update load_everything.sh

--- load_everything.sh
+++ load_everything.sh
@@ -1,4 +1,4 @@
 wget "https://drive.google.com/uc?export=download&id=1nNB-MHGkVLWj1zlOcsDVGzyhkrsvw7UY" -O './DM-GAN+CL/models/netG_epoch_200.pth'
 
 python google_drive.py 1rSnbIGNDGZeHlsUlLdahj0RJ9oo6lgH9 ./DM-GAN+CL/data/coco.zip
-unzip ./DM-GAN+CL/data/coco.zip ./DM-GAN+CL/data/
+unzip ./DM-GAN+CL/data/coco.zip ./DM-GAN+CL/data
